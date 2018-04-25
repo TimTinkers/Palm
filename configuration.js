cb27337a62ee74b0d9a4f07a833a7f509bf8bbce
Timothy Clancy <clancyt@seas.upenn.edu>
2018-04-25 05:38:33 -0400

    Added empty truffle.js configuration file.

---
+++ configuration.js
@@ -0,0 +1,14 @@
+/*
+ * A file to set configuration options for the truffle deployment process.
+ * 
+ * @author Tim Clancy
+ * @version 1.0.0
+ * @date 12.1.2017
+ */
+
+// This configuration option specifies the 12 word mnemonic secret of the 
+// account to use.
+exports.mnemonic = "";
+
+// This configuration option specifies the URL of the node to deploy on.
+exports.nodeURL = "";
